d5b69b2b9d3f30e89b4074368ab2fcff450c8ec9
Viktor <gollwyd@mail.ru>
2019-07-12 22:50:19 +0300

    My version

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hellow');
+let money;
+let time;
+money = prompt("What is your budget?", "20000");
+time = prompt("Enter a dateand time with format YYYY-MM-DD", "2100-12-12");
+let appData = {
+    budget: money,
+    timeData: time
+
+};
